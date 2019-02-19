557b7608c6b641b8a154608b38a58e8331eb4074
[saracorper] <[cordalpernassl@gmail.com]>
2019-02-19 11:52:26 +0100

    Ordenación de archivos

---
+++ MySQL/script.sql
@@ -0,0 +1,134 @@
+-- MySQL dump 10.13  Distrib 5.7.25, for Linux (x86_64)
+--
+-- Host: localhost    Database: bancos
+-- ------------------------------------------------------
+-- Server version	5.7.25-0ubuntu0.18.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Direccion`
+--
+
+DROP TABLE IF EXISTS `Direccion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Direccion` (
+  `direccion_id` int(10) NOT NULL,
+  `Calle` varchar(50) NOT NULL,
+  `Numero` varchar(20) NOT NULL,
+  `Piso` varchar(15) NOT NULL,
+  `Codigo_postal` varchar(15) NOT NULL,
+  `municipio_id` int(10) NOT NULL,
+  PRIMARY KEY (`direccion_id`),
+  KEY `fk_Direccion_Mnunicipio_idx` (`municipio_id`),
+  CONSTRAINT `fk_Direccion_Mnunicipio` FOREIGN KEY (`municipio_id`) REFERENCES `Municipio` (`municipio_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Direccion`
+--
+
+LOCK TABLES `Direccion` WRITE;
+/*!40000 ALTER TABLE `Direccion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Direccion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Municipio`
+--
+
+DROP TABLE IF EXISTS `Municipio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Municipio` (
+  `municipio_id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) NOT NULL,
+  `Codigo` int(11) NOT NULL,
+  `provincia_id` int(11) NOT NULL,
+  PRIMARY KEY (`municipio_id`),
+  KEY `fk_Municipio_Provincia_idx` (`provincia_id`),
+  CONSTRAINT `fk_Municipio_Provincia` FOREIGN KEY (`provincia_id`) REFERENCES `Provincia` (`provincia_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Municipio`
+--
+
+LOCK TABLES `Municipio` WRITE;
+/*!40000 ALTER TABLE `Municipio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Municipio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Pais`
+--
+
+DROP TABLE IF EXISTS `Pais`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Pais` (
+  `pais_id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) NOT NULL,
+  `Codigo` varchar(5) NOT NULL,
+  PRIMARY KEY (`pais_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Pais`
+--
+
+LOCK TABLES `Pais` WRITE;
+/*!40000 ALTER TABLE `Pais` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Pais` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Provincia`
+--
+
+DROP TABLE IF EXISTS `Provincia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Provincia` (
+  `provincia_id` int(10) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `codigo` int(10) NOT NULL,
+  `pais_id` int(10) NOT NULL,
+  PRIMARY KEY (`provincia_id`),
+  KEY `fk_Provincia_Pais_idx` (`pais_id`),
+  CONSTRAINT `fk_Provincia_Pais` FOREIGN KEY (`pais_id`) REFERENCES `Pais` (`pais_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Provincia`
+--
+
+LOCK TABLES `Provincia` WRITE;
+/*!40000 ALTER TABLE `Provincia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Provincia` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-02-11 20:39:50
